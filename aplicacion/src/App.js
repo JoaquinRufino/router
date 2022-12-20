c588100980eede27f59ba6cc28d4aa15fa44334d
Joaquin Rufino <rufinojoaquin10@gmail.com>
2022-12-19 22:15:51 -0300

    "App"

--- aplicacion/src/App.js
+++ aplicacion/src/App.js
@@ -2,18 +2,20 @@ import './App.css';
 import {Routes,Route} from 'react-router-dom';
 import Home from './componentes/Home';
 import Prueba from './componentes/Prueba';
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return(
-  <div>
-      <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/prueba" element={<Prueba/>} />
-      </Routes>
-  </div>
-  );
-  }
-
+    <div>
+  <BrowserRouter> 
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/prueba" element={<Prueba/>} />
+        </Routes>
+  </BrowserRouter>
+    </div>
+    );
+    }
   
 
 export default App;
